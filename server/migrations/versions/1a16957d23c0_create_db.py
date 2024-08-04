9c4b7590c5e071bd92a987e4cf8a3db5cf74c021
ChaseTeague <teaguechase@yahoo.com>
2024-08-04 19:46:47 -0400

    created 3 base models in models.py

---
+++ server/migrations/versions/1a16957d23c0_create_db.py
@@ -0,0 +1,24 @@
+"""Create DB
+
+Revision ID: 1a16957d23c0
+Revises: 
+Create Date: 2024-08-04 18:38:38.551138
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '1a16957d23c0'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
--- server/models.py
+++ server/models.py
@@ -1,6 +1,35 @@
 from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy.ext.associationproxy import association_proxy
 
-from config import db
+from config import db, bcrypt
 
 # Models go here!
+class User(db.Model, SerializerMixin):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String, nullable=False)
+    profile_pic = db.Column(db.String)
+    _password_hash = db.Column(db.String)
+    created_at = db.Column(db.DateTime)
+
+
+class Post(db.Model, SerializerMixin):
+    __tablename__ = 'posts'
+
+    id = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String, nullable=False)
+    body = db.Column(db.String)
+    price = db.Column(db.Integer, nullable=False)
+    created_at = db.Column(db.DateTime)
+
+
+class Review(db.Model, SerializerMixin):
+    __tablename__ = 'reviews'
+
+    id = db.Column(db.Integer, primary_key = True)
+    body = db.Column(db.String, nullable=False)
+    rating = db.Column(db.Integer)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    post_id = db.Column(db.Integer, db.ForeignKey('posts.id'))
+    created_at = db.Column(db.DateTime)
